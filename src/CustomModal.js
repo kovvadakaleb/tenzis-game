4649a6443650d8570cc87d82e85ea5834a00ebf4
Kaleb <kaleb792762@gmail.com>
2024-07-18 18:39:03 +0530

    media queries added for timer and rolls and modal

--- src/CustomModal.js
+++ src/CustomModal.js
@@ -4,15 +4,13 @@ Modal.setAppElement("#root")
 
 function CustomModal(props){
   return(
-    <Modal isOpen={props.isOpen} onRequestClose={props.onClose} 
-           style={{overlay:{backgroundColor:'rgba(0,0,0,0.5)'},
-                   content:{top:'40%',bottom:'auto',left:'50%',right:'50%',transform:'translate(-50%,-50%)',marginRight:'-50%',textAlign:'center'}
-                  }} 
+    <Modal className="modal-content" overlayClassName="modal-overlay"  isOpen={props.isOpen} onRequestClose={props.onClose} 
+           
 
     >
     <h2>{props.modalMessage}</h2>
     
-    <button onClick={props.onClose} style={{padding:'10px 20px',fontSize:'16px',backgroundColor:'lightblue',marginLeft:'20px',border:'none',cursor:'pointer'}}>Close</button>
+    <button onClick={props.onClose} className="modal-button">close</button>
     
     </Modal>
   )
